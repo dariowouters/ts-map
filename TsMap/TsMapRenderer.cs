d96366c289fd5c90ccb6a0e99d66433d43eb2e3a
Dario Wouters <dariowouters@gmail.com>
2017-12-11 19:25:03 +0100

    Added dds reader. Drawing some overlays

--- TsMap/TsMapRenderer.cs
+++ TsMap/TsMapRenderer.cs
@@ -189,6 +189,18 @@ namespace TsMap
                 var cityFont = new Font("Arial", 80 * scaleX, FontStyle.Bold);
                 g.DrawString(city.CityName, cityFont, _palette.CityName, (city.X - startX) * scaleX, (city.Z - startY) * scaleY);
             }
+
+            var overlays = itemsNearby.Where(item => item.Type == TsItemType.MapOverlay);
+
+            foreach (var overlayItem in overlays)
+            {
+                var overlay = _mapper.LookupOverlay(overlayItem.OverlayId);
+                if (overlay == null) continue;
+                Bitmap b = overlay.GetBitmap();
+                g.DrawImage(b, (overlayItem.X - startX) * scaleX, (overlayItem.Z - startY) * scaleY, b.Width * scaleX, b.Height * scaleY);
+            }
+
+
         }
     }
 }
