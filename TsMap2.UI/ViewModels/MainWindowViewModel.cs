412b1fcc83b00dc0f8444daeffdbde3bee6c024e
JAGFx <contact@jagfx.fr>
2021-05-21 18:28:37 +0200

    [TsMap2.UI] add splash screen

--- TsMap2.UI/ViewModels/MainWindowViewModel.cs
+++ TsMap2.UI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
-﻿namespace TsMap2.UI.ViewModels {
+﻿using ReactiveUI.Fody.Helpers;
+
+namespace TsMap2.UI.ViewModels {
     public class MainWindowViewModel : ViewModelBase {
         public string Greeting => "Welcome to Avalonia!";
+
+        [ Reactive ] public bool AppLoaded { get; set; } = false;
     }
 }
--- TsMap2.UI/Views/MainWindow.axaml.cs
+++ TsMap2.UI/Views/MainWindow.axaml.cs
@@ -1,12 +1,19 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
+using Serilog;
 using TsMap2.UI.ViewModels;
 
 namespace TsMap2.UI.Views {
-    public class MainWindow : ReactiveWindow< MainWindowViewModel > {
+    public sealed class MainWindow : ReactiveWindow< MainWindowViewModel > {
         public MainWindow() {
             this.InitializeComponent();
+
 #if DEBUG
             this.AttachDevTools();
 #endif
@@ -14,6 +21,34 @@ namespace TsMap2.UI.Views {
 
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load( this );
+        }
+
+        protected override void OnGotFocus( GotFocusEventArgs e ) {
+            base.OnGotFocus( e );
+
+            // Console.Write( "D >> " );
+            // Console.WriteLine( ViewModel );
+
+            try {
+                Task? t = Task.Run( this.Init );
+                t.ContinueWith( e => this.PostInit() );
+                // t.Wait();
+            } catch ( Exception ex ) {
+                Console.WriteLine( ex.GetBaseException().Message, ex.GetBaseException().StackTrace );
+            }
+        }
+
+        private void Init() {
+            Dispatcher.UIThread.InvokeAsync( () => {
+                Thread.Sleep( 5000 );
+                this.ViewModel.AppLoaded = true;
+                Log.Debug( "UI Loaded {0}", this.ViewModel.AppLoaded );
+            } );
+        }
+
+        private void PostInit() {
+            // Console.Write( "D2 >> " );
+            // Console.WriteLine( ViewModel.AppLoaded );
         }
     }
 }
