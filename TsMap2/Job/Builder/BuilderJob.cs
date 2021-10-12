d64ebc25586edeeef16e2336b4b46438076a66fa
JAGFx <contact@jagfx.fr>
2021-10-12 20:27:53 +0200

    :bug: #15 [TsMap2] skip cities without coordinates on export of file and tiles

--- TsMap2/Job/Builder/BuilderJob.cs
+++ TsMap2/Job/Builder/BuilderJob.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Serilog;
 using TsMap2.Model.TsMapItem;
 
@@ -10,15 +11,24 @@ namespace TsMap2.Job.Builder {
             foreach ( KeyValuePair< ulong, TsMapCityItem > pair in Store().Map.Cities ) {
                 TsMapCityItem item = pair.Value;
                 TsNode?       node = Store().Map.GetNodeByUid( item.NodeUid );
-                ( float x, float z ) = node == null
-                                           ? ( item.X, item.Z )
-                                           : ( node.X, node.Z );
 
-                item.City.X = x;
-                item.City.Y = z;
+                if ( node == null ) {
+                    Log.Debug( "[Job][Builder] City '{0}' skipped and will be removed", item.City.Name );
+                    continue;
+                }
+
+                ( float x, float z ) = ( node.X, node.Z );
+                item.City.X          = x;
+                item.City.Y          = z;
 
                 Log.Debug( "[Job][Builder] City '{0}' at {1},{2}", item.City.Name, item.City.X, item.City.Y );
             }
+
+            // FIXME: Check if it's possible to read data from Node
+            Store().Def.Cities = Store()
+                                 .Def.Cities
+                                 .Where( kv => kv.Value.X != 0 && kv.Value.Y != 0 )
+                                 .ToDictionary( kv => kv.Key, kv => kv.Value );
 
             Log.Information( "[Job][Builder] Done" );
         }
